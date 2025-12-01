0622cd65d05c9d3b90de85b3e194e86f9d1fd929
thestoicrohit <thestoicrohit@gmail.com>
2025-12-01 13:07:00 +0530

    Add files via upload

---
+++ leftpyramid.c
@@ -0,0 +1,24 @@
+/*wap which will print the below mentioned structure * 
+                                                     * *
+                                                     * * *
+                                                     * * * *
+                                                     * * * * * 
+*/
+
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    
+    for(i=1;i<=5;i++)
+    {
+    for(j=1;j<=5;j++)
+    {
+    printf("* ");
+    }
+    printf("\n");
+}
+}
+
+#include<stdio.h>
+void main()
