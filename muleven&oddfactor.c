59d402d60371433284bfd26010c15f5a568c450a
thestoicrohit <thestoicrohit@gmail.com>
2025-12-02 12:59:33 +0530

    Add files via upload

---
+++ muleven&oddfactor.c
@@ -0,0 +1,4 @@
+//wap which will take inpur from the user and find multiplication of all the even numbers and odd factors of that numbers
+#include<stdio.h>
+void main()
+//wap which will print the below mentioned structure
