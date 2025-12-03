aaf54afa066393bdb6c53da8d06c97254530a28c
thestoicrohit <thestoicrohit@gmail.com>
2025-12-03 13:56:38 +0530

    Add files via upload

---
+++ binaryfn.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int binary_search(int arr[],int n, int t)
+{
+    int st=0,end=n-1;
+    while(st<=end)
+    {
+        int mid=(st+end)/2
+        if(arr[mid]==k)
+        {
+            return mid;
+        }
+        else if(arr[mid]<k)
+        {
+            st=mid+1;
+        }
+    }
+    else 
+    {
+        st=mid-1;
+    }
+}   
+{
+//find the smallest 
+}
