1befa55583553a1569d71d6d85994651e84c5ad4
thestoicrohit <thestoicrohit@gmail.com>
2025-12-19 23:58:22 +0530

    Add files via upload

---
+++ count-frequwncy-of-element.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n,a[100],x,c=0;
+    int *p=a;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    scanf("%d",&x);
+    for(int i=0;i<n;i++) if(*(p+i)==x) c++;
+    printf("%d",c);
+    return 0;
+}
